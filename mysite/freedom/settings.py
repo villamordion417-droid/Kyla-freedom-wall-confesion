ba11ff786714b8348b2377b91171fb56ccc35127
Dion <your-github-villamordion417@gmail.com>
2026-03-24 17:20:23 +0900

    Crushy cakes hmmkj dw fegr

--- mysite/freedom/settings.py
+++ mysite/freedom/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
