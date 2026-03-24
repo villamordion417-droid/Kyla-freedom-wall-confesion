c978bdd8a977a9c463433a20676865ac2ac2027a
Dion <your-github-villamordion417@gmail.com>
2026-03-24 17:04:11 +0900

    Crushy cakes hmmkj dw fegr

--- mysite/freedom/urls.py
+++ mysite/freedom/urls.py
@@ -1,15 +1,7 @@
-from django.urls import path
 from django.contrib import admin
-from django.contrib.auth.models import User # Add this
-
-# This tiny function creates the admin automatically
-def create_admin():
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword123')
-
-create_admin() # Run it immediately
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # ... keep your other paths here ...
+    path('', include('wall.urls')), # This connects to your app
 ]
